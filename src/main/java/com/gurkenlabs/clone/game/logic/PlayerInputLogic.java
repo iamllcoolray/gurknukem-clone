40c9e93781d3e887b556c5521cc8b73679611f7c
iamllcoolray <llcoolray24@gmail.com>
2025-07-09 00:01:54 -0400

    Player can cast jump with 'W'

--- src/main/java/com/gurkenlabs/clone/game/logic/PlayerInputLogic.java
+++ src/main/java/com/gurkenlabs/clone/game/logic/PlayerInputLogic.java
@@ -1,5 +1,6 @@
 package com.gurkenlabs.clone.game.logic;
 
+import com.gurkenlabs.clone.game.entities.Player;
 import de.gurkenlabs.litiengine.input.Input;
 
 import java.awt.event.KeyEvent;
@@ -12,5 +13,6 @@ public final class PlayerInputLogic {
     public static void init() {
         // make the game exit upon pressing ESCAPE (by default there is no such key binding and the window needs to be shutdown otherwise, e.g. ALT-F4 on Windows)
         Input.keyboard().onKeyPressed(KeyEvent.VK_ESCAPE, e -> System.exit(0));
+        Input.keyboard().onKeyPressed(KeyEvent.VK_W, e -> Player.instance().jump());
     }
 }
