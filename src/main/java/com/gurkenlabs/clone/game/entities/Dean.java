dcb13a06f24c7725bc4089033bb3b170f0ffaf36
iamllcoolray <llcoolray24@gmail.com>
2025-07-09 21:53:55 -0400

    fixed enemy collision box sizing

--- src/main/java/com/gurkenlabs/clone/game/entities/Dean.java
+++ src/main/java/com/gurkenlabs/clone/game/entities/Dean.java
@@ -2,12 +2,10 @@ package com.gurkenlabs.clone.game.entities;
 
 import de.gurkenlabs.litiengine.entities.*;
 
-@EntityInfo(width = 18, height = 18)
-@AnimationInfo(spritePrefix = "Dean")
-@CollisionInfo(collisionBoxWidth = 8, collisionBoxHeight = 16, collision = true)
-// @MovementInfo(velocity = 70)
+@CollisionInfo(collision = true)
 public class Dean extends Creature {
     public Dean(){
+        super("dean");
         this.addController(new EnemyController(this));
     }
 }
--- src/main/java/com/gurkenlabs/clone/game/entities/Jorge.java
+++ src/main/java/com/gurkenlabs/clone/game/entities/Jorge.java
@@ -3,12 +3,10 @@ package com.gurkenlabs.clone.game.entities;
 
 import de.gurkenlabs.litiengine.entities.*;
 
-@EntityInfo(width = 18, height = 18)
-@AnimationInfo(spritePrefix = "Jorge")
-@CollisionInfo(collisionBoxWidth = 8, collisionBoxHeight = 16, collision = true)
-// @MovementInfo(velocity = 70)
+@CollisionInfo(collision = true)
 public class Jorge extends Creature {
     public Jorge(){
+        super("jorge");
         this.addController(new EnemyController(this));
     }
 }
